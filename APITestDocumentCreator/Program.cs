38f9524293bade51296c47d09bc8eabba4baa134
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2024-04-28 21:53:35 -0300

    Basic document formatting and style.

--- APITestDocumentCreator/Program.cs
+++ APITestDocumentCreator/Program.cs
@@ -1,5 +1,4 @@
-﻿using NPOI.SS.Formula;
-using NPOI.XWPF.UserModel;
+﻿using NPOI.XWPF.UserModel;
 
 namespace APITestDocumentCreator
 {
@@ -13,33 +12,55 @@ namespace APITestDocumentCreator
                 XWPFParagraph title = document.CreateParagraph();
                 title.Alignment = ParagraphAlignment.CENTER;
                 title.VerticalAlignment = TextAlignment.CENTER;
+                title.BorderTop = Borders.Single;
+                title.BorderLeft = Borders.Single;
+                title.BorderRight = Borders.Single;
+                title.BorderBottom = Borders.Single;
 
                 XWPFRun titleRun = title.CreateRun();
+                titleRun.FontFamily = "Calibri";
+                titleRun.FontSize = 20;
+                titleRun.IsBold = true;
                 titleRun.SetText("TITLE TEST");
 
-                // API endpoint name
-                XWPFParagraph endpointName = document.CreateParagraph();
-                endpointName.Alignment = ParagraphAlignment.LEFT;
-                endpointName.VerticalAlignment = TextAlignment.CENTER;
+                // Document section
+                XWPFParagraph documentSection = document.CreateParagraph();
+                documentSection.Alignment = ParagraphAlignment.LEFT;
+                documentSection.VerticalAlignment = TextAlignment.CENTER;
 
-                XWPFRun endpointNameRun = endpointName.CreateRun();
-                endpointNameRun.SetText("ENDPOINT TEST");
+                XWPFRun documentSectionRun = documentSection.CreateRun();
+                documentSectionRun.FontFamily = "Calibri";
+                documentSectionRun.FontSize = 14;
+                documentSectionRun.IsBold = true;
+                documentSectionRun.SetColor("44AE2F");
+                documentSectionRun.SetText("SECTION TEST");
 
-                // Endpoint request title
+                // Endpoint request / response title
                 XWPFParagraph endpointRequest = document.CreateParagraph();
                 endpointRequest.Alignment = ParagraphAlignment.CENTER;
                 endpointRequest.VerticalAlignment = TextAlignment.CENTER;
+                endpointRequest.BorderTop = Borders.Single;
+                endpointRequest.BorderLeft = Borders.Single;
+                endpointRequest.BorderRight = Borders.Single;
+                endpointRequest.BorderBottom = Borders.Single;
 
                 XWPFRun endpointRequestRun = endpointRequest.CreateRun();
+                endpointRequestRun.FontFamily = "Calibri";
+                endpointRequestRun.FontSize = 12;
+                endpointRequestRun.IsBold = true;
+                endpointRequestRun.SetColor("297FC2");
                 endpointRequestRun.SetText("REQUEST TEST");
 
-                // Endpoint JSON request
+                // Endpoint JSON request / response
                 XWPFParagraph endpointJSONRequest = document.CreateParagraph();
                 endpointJSONRequest.Alignment = ParagraphAlignment.LEFT;
 
                 XWPFRun endpointJSONRequestRun = endpointJSONRequest.CreateRun();
+                endpointRequestRun.FontFamily = "Calibri";
+                endpointRequestRun.FontSize = 10;
                 endpointJSONRequestRun.SetText("JSON REQUEST TEST");
 
+                // Saves the file in the user's desktop folder for easy access
                 string userFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
                 using (FileStream fs = new ($"{userFolder}\\API_Test_Document.docx", FileMode.Create))
